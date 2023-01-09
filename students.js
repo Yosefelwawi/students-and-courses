09edb17d5ced2d35fcf6c9752c6affa02b6e168c
Yosefelwawi <97986907+Yosefelwawi@users.noreply.github.com>
2023-01-09 14:46:01 +0100

    renderStudents

--- students.js
+++ students.js
@@ -16,4 +16,27 @@ searchStudent.addEventListener('keyup', function () {
     
 });
 
+function renderStudents (student) {
+    let resultat = document.querySelector("resultat");
+    let studentNamn = document.createElement("h3");
+    let div  = document.createElement("div");
+    div.classList.add("studentWrapper");
 
+    let studentCourse = findStudentCourse(student);
+    let credits = studentCourse.reduce(function(a, b){return a + b}, 0);
+
+    studentNamn.innerText = student.firstname +  " " + student.lastname + "(total credits: " + totalCredits + ")";
+    resultat.appendeChild(studentNamn);
+    resultat.appendChild(div);
+
+    let course = courseById (student);
+
+    for (let i = 0; i < course.length; i ++) {
+        let kurserna = document.createElement("div");
+        kurserna.classList.add("kurser");
+
+        div.appendChild("kurserna");
+        kurserna.innertext = course[i].title + "\n" + student.courses[i].startet.semester + " " + student.courses[i].started.year + " / " + student.courses[i].passedcredits + " of " + courses[i].totalCredits + "credits";
+    }
+
+}
